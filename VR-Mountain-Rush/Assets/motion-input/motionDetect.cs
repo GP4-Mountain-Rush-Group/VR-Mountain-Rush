6ff189b3c9a4f5bde02328bb99efa8945a722309
LKT-E <100751615+LKT-E@users.noreply.github.com>
2022-05-30 14:37:25 +0800

    temp save 2

--- VR-Mountain-Rush/Assets/motion-input/motionDetect.cs
+++ VR-Mountain-Rush/Assets/motion-input/motionDetect.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class motionDetect : MonoBehaviour
 {
+
+    float dFinal = 0;
+
     bool xIsNeg = false;
     int xOppCount = 0;
     float xSlot1 = 0;
     float xSlot2 = 0;
     float xFinal = 0;
 
+    bool yIsNeg = false;
+    int yOppCount = 0;
+    float ySlot1 = 0;
+    float ySlot2 = 0;
+    float yFinal = 0;
+
+    bool zIsNeg = false;
+    int zOppCount = 0;
+    float zSlot1 = 0;
+    float zSlot2 = 0;
+    float zFinal = 0;
+
     GameObject obj;
-    float localX;
+    float DeltaLocalX;
+    float oldLocalX;
+    float DeltaLocalY;
+    float oldLocalY;
+    float DeltaLocalZ;
+    float oldLocalZ;
 
     private void Start()
     {
@@ -20,8 +40,10 @@ public class motionDetect : MonoBehaviour
 
     private void Update()
     {
-        localX = obj.transform.localPosition.x;
-        getPosFinal(ref xSlot1,ref xSlot2,ref xFinal,ref xOppCount,ref xIsNeg,localX);
+
+        DeltaLocalX = obj.transform.localPosition.x - oldLocalX;
+        oldLocalX = obj.transform.localPosition.x;
+        getPosFinal(ref xSlot1,ref xSlot2,ref xFinal,ref xOppCount,ref xIsNeg,DeltaLocalX);
 
 
 
@@ -30,22 +52,22 @@ public class motionDetect : MonoBehaviour
 
 
 
-    void getPosFinal(ref float slot1, ref float slot2, ref float final, ref int oppCount, ref bool isNeg,float localP)
+    void getPosFinal(ref float slot1, ref float slot2, ref float final, ref int oppCount, ref bool isNeg,float DeltaLocalP)
     {
 
         if (!isNeg)
         {
             if (oppCount < 2)
             {
-                if (localP> 0)
+                if (DeltaLocalP> 0)
                 {
                     oppCount = 0;
-                    slot1 += localP;
+                    slot1 += DeltaLocalP;
                 }
                 else
                 {
                     oppCount++;
-                    slot2 += localP;
+                    slot2 += DeltaLocalP;
                 }
             }
             else
@@ -60,15 +82,15 @@ public class motionDetect : MonoBehaviour
         {
             if (oppCount < 2)
             {
-                if (localP < 0)
+                if (DeltaLocalP < 0)
                 {
                     oppCount = 0;
-                    slot1 += localP;
+                    slot1 += DeltaLocalP;
                 }
                 else
                 {
                     oppCount++;
-                    slot2 += localP;
+                    slot2 += DeltaLocalP;
                 }
             }
             else
@@ -83,7 +105,12 @@ public class motionDetect : MonoBehaviour
 
     IEnumerator waitToCheckDistance(){
         while(true){
-
+            if (xFinal != 0 && yFinal != 0 && zFinal != 0){
+                dFinal = Mathf.Sqrt(xFinal * xFinal + yFinal * yFinal + zFinal * zFinal);
+                xFinal = 0;
+                yFinal = 0;
+                zFinal = 0;
+            }
 
             yield return new WaitForSeconds(2);
         }
