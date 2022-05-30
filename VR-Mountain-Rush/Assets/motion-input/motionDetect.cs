9b1fac29bdbf6db58e9de2c25118347ebefa6c13
LKT-E <100751615+LKT-E@users.noreply.github.com>
2022-05-31 00:27:09 +0800

    motion v1.0

--- VR-Mountain-Rush/Assets/motion-input/motionDetect.cs
+++ VR-Mountain-Rush/Assets/motion-input/motionDetect.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class motionDetect : MonoBehaviour
 {
 
-    float dFinal = 0;
+    public float dFinal = 0;
 
     bool xIsNeg = false;
     int xOppCount = 0;
     float xSlot1 = 0;
     float xSlot2 = 0;
-    float xFinal = 0;
+    public float xFinal = 0;
 
     bool yIsNeg = false;
     int yOppCount = 0;
     float ySlot1 = 0;
     float ySlot2 = 0;
-    float yFinal = 0;
+    public float yFinal = 0;
 
     bool zIsNeg = false;
     int zOppCount = 0;
     float zSlot1 = 0;
     float zSlot2 = 0;
-    float zFinal = 0;
+    public float zFinal = 0;
 
-    GameObject obj;
+
     float DeltaLocalX;
     float oldLocalX;
     float DeltaLocalY;
@@ -33,20 +33,43 @@ public class motionDetect : MonoBehaviour
     float DeltaLocalZ;
     float oldLocalZ;
 
-    private void Start()
+    public GameObject obj;
+    public float freqAvgSpeed = 0f;
+    float preSpeed = 0f;
+    public float avgSpeed = 0f;
+
+    public float minDistLimit = 0.3f;
+
+
+    void Start()
     {
         StartCoroutine(waitToCheckDistance());
+        StartCoroutine(averageSpeed());
+        StartCoroutine(get3AxisFinal());
     }
 
-    private void Update()
+    IEnumerator get3AxisFinal()
     {
-
-        DeltaLocalX = obj.transform.localPosition.x - oldLocalX;
         oldLocalX = obj.transform.localPosition.x;
-        getPosFinal(ref xSlot1,ref xSlot2,ref xFinal,ref xOppCount,ref xIsNeg,DeltaLocalX);
+        oldLocalY = obj.transform.localPosition.y;
+        oldLocalZ = obj.transform.localPosition.z;
 
+        while (true)
+        {
+            DeltaLocalX = obj.transform.localPosition.x - oldLocalX;
+            oldLocalX = obj.transform.localPosition.x;
+            getPosFinal(ref xSlot1, ref xSlot2, ref xFinal, ref xOppCount, ref xIsNeg, DeltaLocalX);
 
+            DeltaLocalY = obj.transform.localPosition.y - oldLocalY;
+            oldLocalY = obj.transform.localPosition.y;
+            getPosFinal(ref ySlot1, ref ySlot2, ref yFinal, ref yOppCount, ref yIsNeg, DeltaLocalY);
 
+            DeltaLocalZ = obj.transform.localPosition.z - oldLocalZ;
+            oldLocalZ = obj.transform.localPosition.z;
+            getPosFinal(ref zSlot1, ref zSlot2, ref zFinal, ref zOppCount, ref zIsNeg, DeltaLocalZ);
+
+            yield return new WaitForSeconds(0.03f);
+        }
 
     }
 
@@ -63,6 +86,7 @@ public class motionDetect : MonoBehaviour
                 {
                     oppCount = 0;
                     slot1 += DeltaLocalP;
+                    Debug.Log(slot1);
                 }
                 else
                 {
@@ -75,6 +99,7 @@ public class motionDetect : MonoBehaviour
                 final = slot1;
                 slot1 = slot2;
                 slot2 = 0;
+                oppCount = 0;
                 isNeg = true;
             }
         }
@@ -86,6 +111,7 @@ public class motionDetect : MonoBehaviour
                 {
                     oppCount = 0;
                     slot1 += DeltaLocalP;
+                    Debug.Log(slot1);
                 }
                 else
                 {
@@ -98,21 +124,37 @@ public class motionDetect : MonoBehaviour
                 final = slot1;
                 slot1 = slot2;
                 slot2 = 0;
+                oppCount = 0;
                 isNeg = false;
             }
         }
     }
 
-    IEnumerator waitToCheckDistance(){
+    IEnumerator waitToCheckDistance()
+    {
         while(true){
-            if (xFinal != 0 && yFinal != 0 && zFinal != 0){
+            if ((xFinal != 0 && yFinal != 0 )|| (yFinal != 0 && zFinal != 0)|| (xFinal != 0 && zFinal != 0)) {//&& zFinal != 0){
                 dFinal = Mathf.Sqrt(xFinal * xFinal + yFinal * yFinal + zFinal * zFinal);
-                xFinal = 0;
-                yFinal = 0;
-                zFinal = 0;
+                if (dFinal > minDistLimit)
+                {
+                    xFinal = 0;
+                    yFinal = 0;
+                    zFinal = 0;
+                    preSpeed += 0.5f;
+                }
             }
+            
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
 
-            yield return new WaitForSeconds(2);
+    IEnumerator averageSpeed()
+    {
+        while (true) {
+            avgSpeed = (float)preSpeed / (float) freqAvgSpeed;
+            preSpeed = 0;
+
+            yield return new WaitForSeconds(freqAvgSpeed);
         }
     }
 }
