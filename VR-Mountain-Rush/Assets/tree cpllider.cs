e3297a2bff25258e009e8d01740d55eab0a14950
NoVAlMl <100751587+NoVAlMl@users.noreply.github.com>
2022-07-12 01:32:36 +0800

    main ver 0.1.2

---
+++ VR-Mountain-Rush/Assets/tree cpllider.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treecpllider : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TerrainCollider>().enabled=false;
+        GetComponent<TerrainCollider>().enabled=true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
